bcc4229c6b87935ba41cada269cf0426b65f4b47
kannan <kannans89@hotmail.com>
2025-10-01 11:57:15 +0530

    static

---
+++ Day5/Lab01PlayerCounterApp/Lab01PlayerCounterApp/InsiteInternational/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01PlayerCounterApp.InsiteInternational
+{
+    internal class Player
+    {
+        //instance variables
+        private string _name;
+        private int _age;
+        private static int _count;
+
+        public Player(string name, int age) { 
+            _name = name;
+            _age = age;
+            _count += 1;
+            Console.WriteLine("player created(overlaod2)");
+         
+        }
+
+        public Player(string name)
+        {
+            _name = name;
+            _age = 20;
+            _count += 1;
+            Console.WriteLine("player created(overlaod1)");
+        }
+
+        public string Name { 
+           get { return _name; }
+        }
+
+        public int Age { 
+           get { return _age; }
+        }
+
+        public int Count {
+
+            get {
+                return _count;
+            }
+        }
+
+        //static method can only access
+        //static data
+        //static method only accessed via classname
+        public static int GetPlayerCount() {
+            return _count;
+        }
+
+    }
+}
---
+++ Day5/Lab01PlayerCounterApp/Lab01PlayerCounterApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Lab01PlayerCounterApp.InsiteInternational;
+
+namespace Lab01PlayerCounterApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            // CaseStudy1();
+
+            //static method
+
+            Player p1 = new Player("A");
+            Player p2 = new Player("B");
+            Player p3 = new Player("B");
+
+            Console.WriteLine(Player.GetPlayerCount());
+
+            
+
+
+
+        }
+
+        private static void CaseStudy1()
+        {
+            Player p1 = new Player("Maged");
+            PrintInfo(p1, "p1 object");
+
+            Player p2 = new Player("kannan", 30);
+            PrintInfo(p2, "p2 object");
+
+            Player p3 = new Player("Kuzaimah", 30);
+            PrintInfo(p3, "p3 object");
+        }
+
+        static void PrintInfo(Player p, string additionalDetails) {
+
+            Console.WriteLine(additionalDetails);
+            Console.WriteLine("age is "+p.Age+ "name is "+p.Name);
+            Console.WriteLine("counter is " + p.Count);
+            Console.WriteLine();
+
+        }
+    }
+}
