63c0b7d550fdb2e0e532a0d927d1b493ff3b99f1
kannan <kannans89@hotmail.com>
2025-10-07 11:48:47 +0530

    lambdas

---
+++ Day9/Lab01LamdaAndDelegates/Lab01LamdaAndDelegates/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab01LamdaAndDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inline functions
+            Func<int, int, double> addFn = (a,b) => {
+                return a + b;
+            };
+
+            Console.WriteLine(addFn(10,20));
+
+
+            Action<string, int> printName = (name,iter) => {
+                for (int i = 1; i <= iter; i++) {
+                    Console.WriteLine(i+" "+name.ToUpper());
+                }
+            };
+            printName("Ghufran", 10);
+
+         
+
+        }
+
+        static void CaseSTudy2() {
+
+            Action<string, string> actionPtr;
+            actionPtr = PrintDetails;
+            actionPtr.Invoke("Kuzaimah", "Saudi");
+            actionPtr.Invoke("Maged", "Saudi");
+        }
+
+        static void CaseStudy1() {
+
+            Func<int, int, double> fncPtr1;//expects a function return double and 2input
+            fncPtr1 = Mulitply;
+            double result = fncPtr1(10, 5);
+            Console.WriteLine(result);
+
+        }
+
+        static void PrintDetails(string name, string location) {
+
+            Console.WriteLine("name is "+name);
+            Console.WriteLine("locaiton is "+location);
+        
+        }
+        static double Mulitply(int a, int b) { 
+        
+            return a * b;
+        }
+    }
+}
---
+++ Day9/Lab02SimpleDelegate/Lab02SimpleDelegate/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace Lab02SimpleDelegate
+{
+   // delegate void DPrintMessage(string name);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Account acc1--> acc1 expects account object or subclasses of accoutn
+            //ITransaction txn==> txn expects object of a class which implentents ITransaction
+            //DSomeDelegate obj==> obj expects a function with same signature of delegate
+
+
+            Casestudy1();
+
+            // CasStudy2();
+
+
+            // PrintWizard(PrintGoodBye);
+
+           
+
+        }
+
+
+        //callback
+        static void PrintWizard(Action<string> callBack) {
+
+            Console.WriteLine("inside printing wizard");
+            Console.WriteLine("doing printing opertonan..");
+            Console.WriteLine("notifyonce its done");
+            callBack.Invoke("PrintWizard");
+        
+        
+        }
+        private static void CasStudy2()
+        {
+            Action<string> fnPointer = PrintGoodBye;
+            fnPointer += PrintHello;
+            fnPointer += PrintGoodBye;
+
+            fnPointer.Invoke("Maged");
+
+        }
+
+        private static void Casestudy1()
+        {
+            Action<string> fnPointer;// x expects a function with same signature of delegate
+            fnPointer = PrintGoodBye;
+
+            fnPointer.Invoke("Kuzaimah");
+            fnPointer.Invoke("Maged");
+        }
+
+        static void PrintHello(string name) {
+            Console.WriteLine($"Hello says {name}");
+        }
+
+        static void PrintGoodBye(string name) {
+            Console.WriteLine($"Goodbye says {name}");
+        }
+
+        static void Foo() {
+            Console.WriteLine("inside foo methods");
+        }
+
+
+    }
+}
